70e4b2596d4c457a5fe5b3e9f2f5d4f4d8e83514
ZJRamsey <u1756073@unimail.hud.ac.uk>
2019-10-08 12:00:49 +0100

    Create GenericFighter.cs

---
+++ GenericFighter.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+
+public class GenericFighter: MonoBehaviour {
+
+    // Use this for initialization
+    void Start ()
+    {
+        //Set up values
+        
+    }
+    
+    void React (Vector3 direction, float power, int moveType)
+    {
+        //1: Push back
+        //2: Straight up launch
+        
+        //moveType is the style of move the player is being hit by
+        //Different moves trigger different paths
+        //Take the direction to decide whether to launch left or right
+        //Multiply by power from enemy damage
+    }
+    
+    // Update is called once per frame
+    
+    }
+}
